174f891f8e5eddea4bfa8459e9b5d4a97e9f3668
Samuel A. Falvo II <kc5tja@arrl.net>
2017-05-21 23:10:52 -0700

    LSU handles 16-bit writes.

--- cores/KCP53K/cpu2/bench/verilog/lsu_tb.v
+++ cores/KCP53K/cpu2/bench/verilog/lsu_tb.v
@@ -5,7 +5,44 @@
 module lsu_tb();
 	reg	[11:0]	story_to;
 	reg		fault_to;
-	reg		clk_i, reset_i;
+	reg		clk_i, reset_i, we_i, nomem_i, hword_i;
+	reg	[63:0]	addr_i, dat_i;
+
+	wire		busy_o, rwe_o;
+	wire	[63:0]	dat_o;
+
+	wire	[63:0]	wbmadr_o;
+	wire	[15:0]	wbmdat_o;
+	wire		wbmwe_o, wbmstb_o;
+	reg		wbmack_i;
+
+	lsu ls(
+		.clk_i(clk_i),
+		.reset_i(reset_i),
+		.addr_i(addr_i),
+		.we_i(we_i),
+		.nomem_i(nomem_i),
+		.hword_i(hword_i),
+		.busy_o(busy_o),
+		.rwe_o(rwe_o),
+		.dat_o(dat_o),
+		.dat_i(dat_i),
+
+		.wbmadr_o(wbmadr_o),
+		.wbmdat_o(wbmdat_o),
+		.wbmwe_o(wbmwe_o),
+		.wbmstb_o(wbmstb_o),
+		.wbmack_i(wbmack_i)
+	);
+
+	`STANDARD_FAULT
+	`DEFASSERT0(busy, o)
+	`DEFASSERT0(rwe, o)
+	`DEFASSERT(dat, 63, o)
+	`DEFASSERT(wbmadr, 63, o)
+	`DEFASSERT(wbmdat, 15, o)
+	`DEFASSERT0(wbmwe, o)
+	`DEFASSERT0(wbmstb, o)
 
 	always begin
 		#5 clk_i <= ~clk_i;
@@ -16,7 +53,6 @@ module lsu_tb();
 		reset_i <= 1;
 		wait(~clk_i); wait(clk_i); #1;
 		reset_i <= 0;
-		wait(~clk_i); wait(clk_i); #1;
 	end
 	endtask
 
@@ -24,10 +60,79 @@ module lsu_tb();
 		$dumpfile("lsu.vcd");
 		$dumpvars;
 
-		{clk_i, reset_i, story_to, fault_to} <= 0;
+		{wbmack_i, addr_i, dat_i, we_i, nomem_i, hword_i, clk_i, reset_i, story_to, fault_to} <= 0;
 		wait(~clk_i); wait(clk_i); #1;
 
 		reset;
+		assert_busy(0);
+
+		// Given that we're not accessing memory,
+		// the LSU must pass the address through to the
+		// register writeback stage.
+
+		story_to <= 12'h010;
+
+		addr_i <= 64'h1122334455667788;
+		dat_i <= 64'h7766554433221100;
+		we_i <= 0;
+		nomem_i <= 1;
+		wait(~clk_i); wait(clk_i); #1;
+		assert_dat(64'h1122334455667788);
+		assert_rwe(1);
+
+		story_to <= 12'h018;
+
+		addr_i <= 64'h1122334455667788;
+		dat_i <= 64'h7766554433221100;
+		we_i <= 1;
+		nomem_i <= 1;
+		wait(~clk_i); wait(clk_i); #1;
+		assert_dat(64'h1122334455667788);
+		assert_rwe(1);
+
+		// When idle, the LSU must disable writebacks to the register
+		// file.
+
+		story_to <= 12'h020;
+
+		nomem_i <= 0;
+		wait(~clk_i); wait(clk_i); #1;
+		assert_rwe(0);
+
+		// When writing a half-word to memory,
+		// the LSU must initiate and wait for the complete
+		// Wishbone transaction to complete.  Further, it must
+		// disable register write-back.
+
+		story_to <= 12'h040;
+
+		hword_i <= 1;
+		addr_i <= 64'h1122334455667788;
+		dat_i <= 64'h7766554433221100;
+		we_i <= 1;
+		wait(~clk_i); wait(clk_i); #1;
+
+		hword_i <= 0;
+
+		assert_busy(1);
+		assert_wbmadr(64'h1122334455667788);
+		assert_wbmdat(64'h1100110011001100);
+		assert_wbmwe(1);
+		assert_wbmstb(1);
+		assert_rwe(0);
+		
+		wait(~clk_i); wait(clk_i); #1;
+
+		assert_busy(1);
+		assert_wbmstb(0);
+		assert_rwe(0);
+		
+		wbmack_i <= 1;
+
+		wait(~clk_i); wait(clk_i); #1;
+
+		assert_rwe(0);
+		wbmack_i <= 0;
 
 		#100;
 		$stop;
---
+++ cores/KCP53K/cpu2/rtl/verilog/lsu.v
@@ -0,0 +1,73 @@
+`default_nettype none
+`timescale 1ns / 1ps
+
+module lsu(
+	input		clk_i,
+	input		reset_i,
+	input	[63:0]	addr_i,
+	input		we_i,
+	input		nomem_i,
+	input		hword_i,
+	output		busy_o,
+	output		rwe_o,
+	output	[63:0]	dat_o,
+	input	[63:0]	dat_i,
+
+	output	[63:0]	wbmadr_o,
+	output	[15:0]	wbmdat_o,
+	output		wbmwe_o,
+	output		wbmstb_o,
+	output		wbmcyc_o,
+	input		wbmack_i
+);
+	reg	[63:0]	dat_o;
+	reg		rwe_o;
+
+	reg		mt0, mt1, mt2, mt3;	// Master timeslots
+	reg		st0, st1, st2, st3;	// Slave timeslots
+
+	wire		next_mt0 = hword_i | mt1;
+	wire		next_mt1 = mt2;
+	wire		next_mt2 = mt3;
+	wire		next_mt3 = 0;
+
+	wire		next_st0 = hword_i | (st0 & ~wbmack_i) | st1;
+	wire		next_st1 = (st1 & ~wbmack_i) | st2;
+	wire		next_st2 = (st2 & ~wbmack_i) | st3;
+	wire		next_st3 = st3 & ~wbmack_i;
+
+	assign		wbmadr_o = mt0 ? {addr_i[63:1], 1'b0} : 0;
+	assign		wbmdat_o = mt0 ? dat_i[15:0] : 0;
+	assign		wbmwe_o = mt0 ? we_i : 0;
+	assign		wbmstb_o = mt0;
+
+	assign		wbmcyc_o = st0 | st1 | st2 | st3;
+
+	always @(posedge clk_i) begin
+		dat_o <= dat_o;
+		rwe_o <= 0;
+
+		mt0 <= next_mt0;
+		mt1 <= next_mt1;
+		mt2 <= next_mt2;
+		mt3 <= next_mt3;
+
+		st0 <= next_st0;
+		st1 <= next_st1;
+		st2 <= next_st2;
+		st3 <= next_st3;
+
+		if(reset_i) begin
+			dat_o <= 0;
+			{mt0, mt1, mt2, mt3, st0, st1, st2, st3} <= 0;
+		end
+		else begin
+			if(nomem_i) begin
+				dat_o <= addr_i;
+				rwe_o <= 1;
+			end
+		end
+	end
+
+	assign busy_o = wbmcyc_o;
+endmodule
